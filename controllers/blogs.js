75e35e923f02b351ee477cbc927ce0b2e63e9a5d
martiyen <doyenmartin@gmail.com>
2025-01-31 21:56:04 +0100

    changed promises to async/await syntax

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,28 +1,22 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes,
+    likes: body.likes || 0,
   })
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const newBlog = await blog.save()
+  response.status(201).json(newBlog)
 })
 
 module.exports = blogsRouter
